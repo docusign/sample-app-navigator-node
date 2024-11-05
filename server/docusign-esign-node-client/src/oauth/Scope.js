2addb8a9bd1b8704ca8de724ab2548805cd5350e
Andrii Bilous <andrii.m.bilous@sigma.software>
2024-11-05 11:49:35 +0100

    add package with scopes

---
+++ server/docusign-esign-node-client/src/oauth/Scope.js
@@ -0,0 +1,13 @@
+var SIGNATURE = 'signature';
+var EXTENDED = 'extended';
+var IMPERSONATION = 'impersonation';
+var ADM_STORE_UNIFIED_REPO_READ = 'adm_store_unified_repo_read'; 
+
+module.exports = {
+  SIGNATURE: SIGNATURE,
+  EXTENDED: EXTENDED,
+  IMPERSONATION: IMPERSONATION,
+  ADM_STORE_UNIFIED_REPO_READ: ADM_STORE_UNIFIED_REPO_READ 
+};
+
+Object.freeze(module.exports);
